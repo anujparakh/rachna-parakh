171fc4a643709814c15447ea46fb044773a8871a
Anuj Parakh <anuj@anujinfotech.com>
2025-11-23 00:01:46 -0600

    Added 2 abstract ink personal projects

--- src/data/portfolio/personal-projects.ts
+++ src/data/portfolio/personal-projects.ts
@@ -4,6 +4,8 @@ import bullCreek from '@/img/projects/personal-projects/bull-creek.png';
 import girl from '@/img/projects/personal-projects/girl.jpg';
 import inkAbstract1 from '@/img/projects/personal-projects/ink-abstract-1.png';
 import inkAbstract2 from '@/img/projects/personal-projects/ink-abstract-2.png';
+import inkAbstractRow2_1 from '@/img/projects/personal-projects/ink-abstract-row-2-1.png';
+import inkAbstractRow2_2 from '@/img/projects/personal-projects/ink-abstract-row-2-2.png';
 import inkChild from '@/img/projects/personal-projects/ink-child.jpg';
 import oldMan from '@/img/projects/personal-projects/old-man.png';
 import prashant from '@/img/projects/personal-projects/prashant.png';
@@ -58,6 +60,28 @@ export const personalProjectsData: PortfolioProjectData = {
             src: '',
             alt: 'Ink Child',
             data: inkChild,
+          },
+        ],
+        gridColumns: 0,
+        imageHeight: 'auto',
+        imageClass: '!w-auto h-[60vh]',
+      },
+    },
+    {
+      id: 'ink-2nd-row',
+      title: '',
+      type: 'image-grid',
+      content: {
+        images: [
+          {
+            src: '',
+            alt: 'Ink Abstract 3',
+            data: inkAbstractRow2_1,
+          },
+          {
+            src: '',
+            alt: 'Ink Abstract 4',
+            data: inkAbstractRow2_2,
           },
         ],
         gridColumns: 0,
